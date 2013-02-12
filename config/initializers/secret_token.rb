e4b2d94982d0b4094cb6574ebd5a18228f08e52e
Eric Haughee <ehaughee@gmail.com>
2013-02-12 14:56:15 -0500

    Initial commit: - Added emberjs folder hierarchy - Added various dependencies - Added default route

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Krotos::Application.config.secret_token = 'b3846f4fe2eabbf7a55546c300ec6333d636845082154cc7cec5a7bbe17144fa5bb7f2150eeddf134487b9089826164655d26c5e08cb86f448ff46fea96aa067'
